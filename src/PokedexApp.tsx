c94cfb7ce44fba06cc12c9c118b88657b040c4dd
Oscar Espinoza <oscaranto.espinoza@gmail.com>
2026-02-26 00:48:20 -0300

    feat: navigator and screen added

---
+++ src/PokedexApp.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './presentation/navigator/StackNavigator';
+
+export const PokedexApp = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
---
+++ src/presentation/navigator/StackNavigator.tsx
@@ -0,0 +1,22 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { PokemonScreen } from '../screens/pokemon/PokemonScreen';
+import { SearchScreen } from '../screens/search/SearchScreen';
+
+export type RootStackParams = {
+  HomeScreen: undefined;
+  PokemonScreen: { pokemonId: number };
+  SearchScreen: undefined;
+};
+
+const Stack = createStackNavigator();
+
+export const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Pokemon" component={PokemonScreen} />
+      <Stack.Screen name="Search" component={SearchScreen} />
+    </Stack.Navigator>
+  );
+};

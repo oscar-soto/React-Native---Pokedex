d19c24b8448085f2be7cf969949bd8db348dbc4e
Oscar Espinoza <oscaranto.espinoza@gmail.com>
2026-02-27 17:09:39 -0300

    feat: cache pokemon request

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { FlatList, StyleSheet, View } from 'react-native';
 import { Text } from 'react-native-paper';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { getPokemons } from '../../../actions/pokemons';
@@ -10,6 +10,7 @@ import { PokemonCard } from '../../components/pokemon';
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
+  const queryClient = useQueryClient();
 
   // Traditional form to make a htpp request
   // const { data: pokemons } = useQuery({
@@ -21,9 +22,15 @@ export const HomeScreen = () => {
   const { data, fetchNextPage } = useInfiniteQuery({
     queryKey: ['pokemons', 'infinite'],
     initialPageParam: 0,
-    queryFn: params => getPokemons(params.pageParam),
-    getNextPageParam: (lastPage, pages) => pages.length,
     staleTime: 1000 * 60 * 60, // 60 minutes
+    queryFn: async params => {
+      const pokemons = await getPokemons(params.pageParam);
+      pokemons.forEach(pokemon => {
+        queryClient.setQueryData(['pokemon', pokemon.id], pokemon);
+      });
+      return pokemons;
+    },
+    getNextPageParam: (lastPage, pages) => pages.length,
   });
 
   return (
